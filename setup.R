e4f1cf8274b986f8a53a72a62f2b7bf8cc07fb05
David <d@tdfd.org>
2019-01-30 18:35:59 -0500

    create /data/ and /docs/, functionalize load_tables, create analysis.Rmd

--- setup.R
+++ setup.R
@@ -1,68 +1,60 @@
-## This file allows for the data environment to be set up
-## consistently for different users. All data will live in
-## the /data/ folder
-
-# REQUIRED PACKAGES
-
-if(!require(pacman)) install.packages("pacman"); require(pacman)
-pacman::p_load(tidyverse, lubridate, odbc)
-
-
-# REQUIRED TABLES
-
-tables <- c(
-    "DIBI_Permits", "A4A_Monthly_Cargo_Traffic"
-)
-
-
-
-
-
-
-
-
-
-
-
-
-# create SQL Server connection
-connection <- dbConnect(odbc(),
-                        Driver = "SQL Server",
-                        Server = "freightwaves.ctaqnedkuefm.us-east-2.rds.amazonaws.com",
-                        Database = "Warehouse",
-                        UID = "fwdbmain",
-                        PWD = "7AC?Ls9_z3W#@XrR",
-                        Port = 1433)
-
-# set working directory to the data folder
-setwd("./data/")
-for (i in tables) {
-
-    filename <- paste0(i, ".csv")
+load_tables <- function(tables) {
+    ## This file allows for the data environment to be set up
+    ## consistently for different users. All data will live in
+    ## the /data/ folder
     
-    if (!file.exists(filename)) {
-        
-        # execute SQL query for all data in the table
-        tmp <- dbGetQuery(connection, paste0("SELECT * FROM Warehouse.dbo.", i))
-        
-        # write a .csv of the data in the /data/ folder
-        write_csv(tmp, path = filename)
-        
-        # store the data into memory
-        assign(i, value = tmp)
-        rm(tmp)
-        
-    } else {
-        
-        read_csv(filename) %>%
-            assign(i, value = .)
-        
+    # REQUIRED PACKAGES
+    
+    if(!require(odbc)) install.packages("odbc"); require(odbc)
+    if(!require(readr)) install.packages("readr"); require(readr)
+    
+    
+    if (!is.vector(tables) & !is.list(tables)) {
+        stop("Please provide a vector or list of table names in Warehouse.dbo.")
     }
+    
+    # create SQL Server connection
+    connection <- dbConnect(odbc(),
+                            Driver = "SQL Server",
+                            Server = "freightwaves.ctaqnedkuefm.us-east-2.rds.amazonaws.com",
+                            Database = "Warehouse",
+                            UID = "fwdbmain",
+                            PWD = "7AC?Ls9_z3W#@XrR",
+                            Port = 1433)
+    
+    # set working directory to the data folder
+    setwd("./data/raw/")
+    for (i in tables) {
+        
+        filename <- paste0(i, ".csv")
+        
+        if (!file.exists(filename)) {
+            
+            # execute SQL query for all data in the table
+            tmp <- dbGetQuery(connection, paste0("SELECT * FROM Warehouse.dbo.", i))
+            
+            # write a .csv of the data in the /data/ folder
+            write_csv(tmp, path = filename)
+            
+            # store the data into memory
+            assign(i, value = tmp)
+            rm(tmp)
+            
+        } else {
+            
+            tmp <- read_csv(filename)
+            assign(i, value = tmp)
+            rm(tmp)
+            
+        }
+    }
+    
+    # return the working directory to the repo folder
+    setwd("../..")
+    
+    
+    dbDisconnect(connection)
+    rm(connection)
 }
 
-# return the working directory to the repo folder
-setwd("..")
 
-
-dbDisconnect(connection)
-rm(connection)
