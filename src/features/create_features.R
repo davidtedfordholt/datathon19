e71e696a52c9214a12fb83ded3efa65225845bd5
David <d@tdfd.org>
2019-02-04 17:27:11 -0500

    add feature suggestions

--- src/features/create_features.R
+++ src/features/create_features.R
@@ -7,10 +7,36 @@ data <- read_feather("./data/raw/")
 # ext <- read_csv("./data/external/")
 
 
-Transformations
+
+
+# MISSING DATA:
+#     IF you're going to impute:
+#       KNN, trees, linear, MICE, missForest, Hmisc, mi, Amelia
+#     Otherwise, consider encoding missingness
+# 
+# CATEGORICAL:
+#     Factors or Dummy variables
+#     Hashing
+#     Compress rare
+#     Effect/Likelihood encoding
+#     
+# ORDINAL:
+#     Polynomial contrast
+#     
+# NUMERIC:
+#     Transformations (Box-Cox, smoothing, )
+#     Expansions (splines/knots, loess/GAM, hinge, binning)
+#     Multi-dimensional (linear projection, PCA/ICA/Kernel PCA, autoencoders, matrix factorization, distance/depth)
+#     
+# INTERACTIONS:
+#     LASSO
+#     Two-stage modeling
+#     Trees
+#     Cubist
 
 
 
+# Is your data centered & scaled?
 
 
 # SAVE PROCESSED DATA
