c1a8a1bef88f6d2cf33df7dd652d0051ca334d57
David <d@tdfd.org>
2019-02-06 09:20:37 -0500

    make silent by default

--- src/data/create_dataset.R
+++ src/data/create_dataset.R
@@ -1,8 +1,7 @@
-if (!require(pacman)) install.packages("pacman"); require(pacman)
-pacman::p_load(tidyverse, lubridate, feather)
+# pacman::p_load(tidyverse, lubridate, feather)
 
 # READ IN RAW/EXT DATA
-data <- read_feather("./data/raw/")
+# data <- read_feather("./data/raw/")
 # data <- read_csv("./data/raw/")
 # ext <- read_csv("./data/external/")
 
@@ -13,5 +12,5 @@ data <- read_feather("./data/raw/")
 
 
 # SAVE INTERIM DATA
-write_feather(data, "./data/interim//")
+# write_feather(data, "./data/interim//")
 # write_csv(data, "./data/interim")
